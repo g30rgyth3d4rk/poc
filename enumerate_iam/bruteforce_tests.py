aec64c2a4367fa48e429ef7afae29f60b47e2510
g30rgyth3d4rk <georgemaina197@gmail.com>
2022-07-15 15:51:34 +0300

    First

---
+++ enumerate_iam/bruteforce_tests.py
@@ -0,0 +1,36 @@
+BRUTEFORCE_TESTS = {
+ "sts": [
+        "get_caller_identity",
+        "get_session_token"
+    ],
+ "dynamodb": [
+        "describe_endpoints",
+        "describe_limits",
+        "list_backups",
+        "list_global_tables",
+        "list_tables",
+        "list_tables"
+    ],
+"iam": [
+        "get_account_authorization_details",
+        "get_account_password_policy",
+        "get_account_summary",
+        "get_credential_report",
+        "get_user",
+        "list_access_keys",
+        "list_account_aliases",
+        "list_groups",
+        "list_instance_profiles",
+        "list_mfa_devices",
+        "list_open_id_connect_providers",
+        "list_policies",
+        "list_roles",
+        "list_saml_providers",
+        "list_server_certificates",
+        "list_service_specific_credentials",
+        "list_signing_certificates",
+        "list_ssh_public_keys",
+        "list_users",
+        "list_virtual_mfa_devices"
+    ]
+}
